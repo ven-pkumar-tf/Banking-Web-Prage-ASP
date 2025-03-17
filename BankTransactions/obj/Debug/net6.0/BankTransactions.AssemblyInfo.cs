6bff86f4c3edcecf6fc2ce780eff7bdc4cf85200
ven-pkumar-tf <pasupathi.kumar@c2stechs.com>
2025-03-17 23:40:23 +0530

    sa

--- BankTransactions/obj/Debug/net6.0/BankTransactions.AssemblyInfo.cs
+++ BankTransactions/obj/Debug/net6.0/BankTransactions.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BankTransactions")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad29e2e3b87c6fe75e9c7510633eb02804460ea7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67326d64fb89cdf4bfb7f26419a90aa80a72d679")]
 [assembly: System.Reflection.AssemblyProductAttribute("BankTransactions")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BankTransactions")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
